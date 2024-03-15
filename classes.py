1cfccca9ae28aec37eb539edb88800828ea5f119
João Victor Santos <29289656+Kotarou0@users.noreply.github.com>
2024-03-15 07:31:43 -0300

    Classes criadas com variáveis definidas

--- classes.py
+++ classes.py
@@ -11,3 +11,31 @@ class Funcionario(Pessoa):
         super().__init__(nome, cpf, endereco, telefone, email)
         self.salario = salario
         self.senha = senha
+
+class Dono(Funcionario):
+    def __init__(self, nome, cpf, endereco, telefone, email, salario, senha):
+        super().__init__(nome, cpf, endereco, telefone, email, salario, senha)
+
+class Publicacao:
+    def __init__(self, autor, editora, ano, titulo, genero, isbn):
+        self.autor = autor
+        self.editora = editora
+        self.ano = ano
+        self.titulo = titulo
+        self.genero = genero
+        self.isbn = isbn
+
+class Livro(Publicacao):
+    def __init__(self, autor, editora, ano, titulo, genero, isbn, volume):
+        super().__init__(autor, editora, ano, titulo, genero, isbn)
+        self.volume = volume
+
+class Revista(Publicacao):
+    def __init__(self, autor, editora, ano, titulo, genero, isbn, semana):
+        super().__init__(autor, editora, ano, titulo, genero, isbn)
+        self.semana = semana
+
+class Jornal(Publicacao):
+    def __init__(self, autor, editora, ano, titulo, genero, isbn, dia):
+        super().__init__(autor, editora, ano, titulo, genero, isbn)
+        self.dia = dia
