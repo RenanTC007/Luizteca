1bb79c9a767e306646c49000759970a0a2ade902
João Victor Santos <29289656+Kotarou0@users.noreply.github.com>
2024-03-15 09:11:12 -0300

    Adicionada a função de: Mudar senha; Adicionar clientes; Demitir funcionários.

--- classes.py
+++ classes.py
@@ -1,3 +1,6 @@
+from getpass import getpass
+from hashlib import sha256
+
 SENHA_PADRAO = "a665a45920422f9d417e4867efdc4fb8a04a1f3fff1fa07e998e86f7f7a27ae3" # 123
 TETO_SALARIAL = 10000
 
@@ -14,6 +17,19 @@ class Funcionario(Pessoa):
         super().__init__(nome, cpf, endereco, telefone, email)
         self.salario = salario
         self.senha = senha
+
+    def cadastrar_cliente(self):
+        print("Cadastrando um novo cliente.")
+        nome = input("Nome: ")
+        cpf = input("CPF: ")
+        endereco = input("Endereço: ")
+        telefone = input("Telefone: ")
+        email = input("E-mail: ")
+        return Pessoa(nome, cpf, endereco, telefone, email)
+    
+    def mudar_senha(self):
+        print("Mudando sua senha.")
+        self.senha = sha256(getpass("Digite uma nova senha: ").encode()).hexdigest()
 
 class Dono(Funcionario):
     def __init__(self, nome, cpf, endereco, telefone, email, salario, senha):
@@ -34,6 +50,15 @@ class Dono(Funcionario):
                 return Funcionario(nome, cpf, endereco, telefone, email, salario, senha)
             except:
                 print("O salário deve ser um número menor que", TETO_SALARIAL, " Tente novamente.")
+    
+    def remover_funcionario(self, lista_funcionarios):
+        while True:
+            cpf = input("Digite o CPF do funcionário que você quer demitir: ")
+            for funcionario in lista_funcionarios:
+                if funcionario.cpf == cpf: 
+                    print("Funcionário", funcionario.nome, "demitido com sucesso.")
+                    return funcionario
+            print("Nenhum funcionário encontrado com esse CPF. Tente novamente.")
 
 class Publicacao:
     def __init__(self, autor, editora, ano, titulo, genero, isbn):
--- sistema.py
+++ sistema.py
@@ -26,6 +26,8 @@ def sistema():
     print("Bem-vindo,", funcionario_atual.nome, ". Opções disponíveis: ")
     print("[1] Cadastrar funcionário.")
     print("[2] Demitir funcionário.")
+    print("[3] Mudar sua senha.")
+    print("[4] Cadastrar cliente.")
     print("[99] Sair da conta.")
     condicao = True
     while condicao:
@@ -42,12 +44,21 @@ def sistema():
                 funcionarios.append(dono.cadastrar_funcionario())
             else:
                 print("Você não tem permissões para cadastrar funcionários.")
-    
+        case 2:
+            if funcionario_atual == dono:
+                funcionarios.remove(dono.remover_funcionario(funcionarios))
+            else:
+                print("Você não tem permissão para remover funcionários.")
+        case 3:
+            funcionario_atual.mudar_senha()
+        case 4:
+            clientes.append(funcionario_atual.cadastrar_cliente())
     return escolha
 
 # Cadastrando o dono com a senha padrão
 dono = Dono("Luiz de Moraes Sampaio", "426.704.238-17", "Guarulhos", "(11) 94318-6452", "luiz.sampaio@yahoo.com.br", 2, SENHA_PADRAO)
 funcionarios = [dono]
+clientes = []
 
 while True:
     funcionario_atual = fazer_login()
