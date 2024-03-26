16d9a520a87d0391085e67980c611d9c24d1a21a
João Victor Santos <29289656+Kotarou0@users.noreply.github.com>
2024-03-26 19:23:36 -0300

    Comentários e fixes

--- classes.py
+++ classes.py
@@ -2,7 +2,7 @@ from getpass import getpass
 from hashlib import sha256
 
 SENHA_PADRAO = "a665a45920422f9d417e4867efdc4fb8a04a1f3fff1fa07e998e86f7f7a27ae3" # 123
-TETO_SALARIAL = 10000
+TETO_SALARIAL = 10000 # Máximo valor que um funcionário pode ter de salário.
 
 class Pessoa:
     def __init__(self, nome, cpf, endereco, telefone, email):
@@ -25,28 +25,30 @@ class Funcionario(Pessoa):
         endereco = input("Endereço: ")
         telefone = input("Telefone: ")
         email = input("E-mail: ")
+        # Retorna um objeto Pessoa com os dados acima.
         return Pessoa(nome, cpf, endereco, telefone, email)
     
     def mudar_senha(self):
         print("Mudando sua senha.")
+        # Muda a senha usando GETPASS para esconder a senha e SHA256 para criptografar.
         self.senha = sha256(getpass("Digite uma nova senha: ").encode()).hexdigest()
 
     def adicionar_publicacao(self):
         while True:
             print("Adicionando uma nova publicação.")
             try:
-                tipo = int(input("Tipo da publicação. Digite 1 para LIVRO, 2 para REVISTA e 3 para JORNAL: "))
-                if tipo < 1 or tipo > 3: raise Exception()
+                tipo = int(input("Escolha o tipo da publicação. Digite 1 para LIVRO, 2 para REVISTA e 3 para JORNAL: "))
+                if tipo < 1 or tipo > 3: raise Exception() # Jogar um erro de o tipo não estiver entre 1 e 3.
                 autor = input("Autor: ")
                 editora = input("Editora: ")
                 ano = int(input("Ano de publicação: "))
                 titulo = input("Título: ")
                 genero = input("Gênero: ")
                 isbn = input("ISBN: ")
-
                 break
             except:
                 print("Você digitou algo errado. Tente novamente.")
+        # Retorna um objeto de acordo com o tipo escolhido.
         match tipo:
             case 1:
                 volume = input("Volume: ")
@@ -60,22 +62,17 @@ class Funcionario(Pessoa):
         print("Nova publicação adicionada com sucesso. Agora adicione exemplares desta publicação.")
 
     def adicionar_exemplar(self, lista):
-        print("Adicionando um novo exemplar. Escolha um número correspondente à publicação que está sendo cadastrada:")
-        i = 1
-        for p in lista:
-            print(f"[{i}] {p.titulo}, ISBN {p.isbn}, {p.tipo()}.")
-            i += 1
-        n = 0
         while True:
             try:
                 n = int(input())
-                if n < 1 or n > len(lista): raise Exception()
+                if n < 1 or n > len(lista): raise Exception() # A publicação deve estar na lista de publicações.
                 break
             except:
                 print(f"Digite um número inteiro entre 1 e {len(lista)}. Tente novamente.")
                 pass
-        lista[n-1].exemplares.append(Exemplar())
-        print(f"Exemplar do {lista[n-1].tipo()} {lista[n-1].titulo} adicionado com sucesso.")
+        p = lista[n-1]
+        p.exemplares.append(Exemplar()) # Adiciona um novo objeto Exemplar à publicação.
+        print(f"Exemplar do {p.tipo()} {p.titulo} adicionado com sucesso.")
         
 
 class Dono(Funcionario):
@@ -87,25 +84,26 @@ class Dono(Funcionario):
             try:
                 print("Cadastrando um novo funcionário.")
                 nome = input("Nome: ")
-                cpf = input("Cpf: ")
+                cpf = input("CPF (com pontuação): ")
                 endereco = input("Endereço: ")
                 telefone = input("Número de telefone: ")
                 email = input("E-mail: ")
                 salario = float(input("Salário [apenas números]: "))
+                if salario > TETO_SALARIAL: raise Exception()
                 senha = SENHA_PADRAO
-                print("Funcionário", nome, "cadastrado com sucesso. A senha padrão é 123")
-                return Funcionario(nome, cpf, endereco, telefone, email, salario, senha)
+                print("Funcionário", nome, "cadastrado com sucesso. A senha padrão é 123.")
+                return Funcionario(nome, cpf, endereco, telefone, email, salario, senha) # Retorna um objeto Funcionário
             except:
                 print("O salário deve ser um número menor que", TETO_SALARIAL, " Tente novamente.")
     
     def remover_funcionario(self, lista_funcionarios):
         while True:
             cpf = input("Digite o CPF do funcionário que você quer demitir: ")
-            for funcionario in lista_funcionarios:
-                if funcionario != self:
+            for funcionario in lista_funcionarios: # Procura em todos os funcionários na lista.
+                if funcionario != self: # Não pode demitir o dono.
                     if funcionario.cpf == cpf: 
                         print("Funcionário", funcionario.nome, "demitido com sucesso.")
-                        return funcionario
+                        return funcionario # Retorna o funcionário para ser removido da lista no sistema.py
             print("Nenhum funcionário encontrado com esse CPF. Tente novamente.")
 
 class Publicacao:
@@ -116,7 +114,7 @@ class Publicacao:
         self.titulo = titulo
         self.genero = genero
         self.isbn = isbn
-        self.exemplares = []
+        self.exemplares = [] # Lista de exemplares desta publicação.
 
     def quantidade_exemplares(self):
         return len(self.exemplares)
@@ -128,13 +126,13 @@ class Publicacao:
         return i
 
     def tipo(self):
-        pass
+        pass # Retorna o tipo da publicação. Sobrescrever esta função nas classes derivadas.
 
     def listar_exemplares_emprestados(self):
         i = 1
         for e in self.exemplares:
             if e.emprestado:
-                print(f"[{i}] Emprestado para {e.cliente.nome}.")
+                print(f"[{i}] Emprestado para {e.cliente.nome} no dia {e.data_emprestimo} por {e.funcionario}.")
                 i += 1
 
 class Livro(Publicacao):
@@ -161,14 +159,15 @@ class Jornal(Publicacao):
     def tipo(self):
         return "Jornal"
 
-class Exemplar:
+class Exemplar: # Cada publicação tem uma lista de exemplares com objetos desta classe. Assim é possível ter vários exemplares de uma mesma publicação.
     def __init__(self):
         self.emprestado = False
         self.cliente = None
-    def emprestar_exemplar(self, funcionario, cliente):
+    def emprestar_exemplar(self, funcionario, cliente, data_emprestimo):
         self.funcionario = funcionario
         self.emprestado = True
         self.cliente = cliente
+        self.data_emprestimo = data_emprestimo
     def devolver_exemplar(self, data_devolucao):
         self.data_devolucao = data_devolucao
         self.emprestado = False
--- sistema.py
+++ sistema.py
@@ -4,39 +4,47 @@ import datetime
 from classes import *
 import os
 
+SEM_PERMISSAO = "Você não tem permissão para realizar esta ação."
+
 def clear():
-    os.system("clear")
+    os.system("clear") # Limpa a tela.
 
 def fazer_login():
     print("Bem-vindo. Faça login, ou saia com o atalho CTRL+C.")
-    condicao = True
-    while condicao:
+    while True:
         email = input("Digite seu e-mail: ")
-        senha = sha256(getpass("Digite sua senha: ").encode()).hexdigest()
+        senha = sha256(getpass("Digite sua senha: ").encode()).hexdigest() # Lê a senha sem mostrar na tela com GETPASS e a criptografa com SHA256.
 
         funcionario_atual = 0
-        for funcionario in funcionarios:
+        for funcionario in funcionarios: # Procura um funcionário com o email digitado.
             if funcionario.email == email: funcionario_atual = funcionario
 
-        if funcionario_atual == 0:
+        if funcionario_atual == 0: # Se nenhum funcionário com este email foi encontrado.
             print("Este e-mail não está cadastrado. Tente novamente.")
         else:
             if funcionario_atual.senha == senha: 
                 print("Logado com sucesso.")
-                condicao = False
+                break # Volta pro loop inicial
             else: print("Senha incorreta. Tente novamente.")
     return funcionario_atual
 
 def listar_publicacoes(nome=""):
     i = 1
-    nome = nome.lower()
+    nome = nome.lower() # Não é case sensitive.
     for p in publicacoes:
         if nome in p.titulo.lower():
             print(f"[{i}] {p.titulo}, ISBN {p.isbn}")
         i += 1
 
-def sistema():
-    print("Bem-vindo,", funcionario_atual.nome, ". Opções disponíveis: ")
+def listar_funcionarios():
+    i = 1
+    for fun in funcionarios:
+        print(f"[{i}] {fun.nome}, CPF {fun.cpf}")
+    i += 1
+
+
+def sistema(): # Chamado até sair da conta.
+    print(f"Bem-vindo {funcionario_atual.nome}. Opções disponíveis: ")
     print("[1] Cadastrar funcionário.")
     print("[2] Demitir funcionário.")
     print("[3] Mudar sua senha.")
@@ -47,12 +55,15 @@ def sistema():
     print("[8] Devolver exemplar.")
     print("[9] Buscar publicação por título.")
     print("[10] Listar funcionários.")
+    print("[11] Alterar dados de um funcionário.")
+    print("[12] Listar exemplares emprestados.")
+    print("[13] Multar exemplares não conservados.")
     
     print("[99] Sair da conta.")
     
-    while True:
+    while True: # Tenta pegar uma escolha válida até conseguir.
         try:
-            escolha = int(input("O que você deseja fazer? "))
+            escolha = int(input("Escolha: "))
             break
         except:
             print("Você não digitou um número. Tente novamente.")
@@ -60,25 +71,29 @@ def sistema():
     match escolha:
         case 1:
             if e_dono:
-                funcionarios.append(dono.cadastrar_funcionario())
+                funcionarios.append(dono.cadastrar_funcionario()) # Adiciona o funcionário cadastrado na lista de funcionários.
             else:
-                print("Você não tem permissões para cadastrar funcionários.")
+                print(SEM_PERMISSAO) # Se não for dono, mostrar que não tem permissão.
         case 2:
             if e_dono:
-                if len(funcionarios) == 1: print("Não há outros funcionários cadastrados.")
-                else: funcionarios.remove(dono.remover_funcionario(funcionarios))
+                if len(funcionarios) == 1: 
+                    print("Não há outros funcionários cadastrados.") # Apenas o dono está cadastrado.
+                else:
+                    funcionarios.remove(dono.remover_funcionario(funcionarios)) # Remove o funcionário escolhido.
             else:
-                print("Você não tem permissão para remover funcionários.")
+                print(SEM_PERMISSAO)
         case 3:
-            funcionario_atual.mudar_senha()
+            funcionario_atual.mudar_senha() # Função da classe Funcionario.
         case 4:
-            clientes.append(funcionario_atual.cadastrar_cliente())
+            clientes.append(funcionario_atual.cadastrar_cliente()) # Adiciona um cliente novo na lista de clientes.
         case 5:
-            nova_publicacao = funcionario_atual.adicionar_publicacao()
-            publicacoes.append(nova_publicacao)
+            publicacoes.append(funcionario_atual.adicionar_publicacao()) # Adiciona uma publicação nova na lista de publicações.
         case 6:
-            if len(publicacoes) == 0: print("Não há publicações cadastradas. Por favor, cadastre uma publicação primeiro.")
+            if len(publicacoes) == 0: # 0 publicações cadastradas, não dá para adicionar exemplares.
+                print("Não há publicações cadastradas. Por favor, cadastre uma publicação primeiro.")
             else: 
+                print("Adicionando um novo exemplar. Escolha um número correspondente à publicação que está sendo cadastrada:")
+                listar_publicacoes()
                 funcionario_atual.adicionar_exemplar(publicacoes)
         case 7:
             while True:
@@ -88,13 +103,14 @@ def sistema():
                         break
                     listar_publicacoes()
                     n = int(input("Escolha uma dessas publicações para emprestar: "))
-                    if n < 1 or n > len(publicacoes): raise Exception()
+                    if n < 1 or n > len(publicacoes): raise Exception() # Verifica se o número escolhido corresponde a uma publicação.
 
-                    p = publicacoes[n-1]
-                    if p.quantidade_exemplares_emprestados() == p.quantidade_exemplares():
+                    p = publicacoes[n-1] # -1 porque listamos as publicações a partir do 1.
+                    if p.quantidade_exemplares_emprestados() == p.quantidade_exemplares(): # Todos foram emprestados.
                         print("Não há exemplares disponíveis para esta publicação.")
                         break
                     else:
+                        # Pede-se o CPF por motivos de segurança: não é muito legal o funcionário ter acesso a todos os clientes.
                         cpf = int(input("Qual cliente quer pegar este título emprestado? Digite o CPF (com pontuação): "))
                         cliente = 0
                         for c in clientes:
@@ -104,9 +120,9 @@ def sistema():
                             raise Exception()
                         for e in p.exemplares:
                             if e.emprestado == False:
-                                e.emprestar_exemplar(funcionario_atual)
+                                e.emprestar_exemplar(funcionario_atual, datetime.datetime.now())
                                 break
-                        print(f"Exemplar de {p.titulo} emprestado para {cliente.nome} com sucesso.")
+                        print(f"Exemplar de {p.titulo} emprestado para {cliente.nome} com sucesso no dia {datetime.datetime.now()} com sucesso.")
                         break
                 except:
                     print("Você digitou um número fora do intervalo permitido ou não digitou um número. Tente novamente.")
@@ -128,7 +144,14 @@ def sistema():
                     if n < 1 or n > p.quantidade_exemplares_emprestados():
                         raise Exception()
                     e = p.exemplares[n-1]
+                    estado = int(input("Qual é o estado de conservação deste exemplar?\n[1] Conservado.\n[2] Não conservado."))
+                    if estado < 1 or estado > 2: raise Exception()
                     e.devolver_exemplar(datetime.datetime.now())
+                    if estado == 2: 
+                        print("Uma multa será aplicada ao cliente de acordo com o dono da biblioteca.")
+                        print("Exemplar removido do sistema.")
+                        multar.append([p, e])
+                        p.exemplares.remove(e)
                     print("Exemplar devolvido em", datetime.datetime.now(),"com sucesso.")
                     break
                 except:
@@ -139,12 +162,50 @@ def sistema():
             listar_publicacoes(nome)
         case 10:
             if e_dono:
-                # Listar funcionários aqui.
-                pass
+                print("Os funcionários cadastrados são:")
+                listar_funcionarios()
             else:
-                print("Você não tem permissão para realizar esta ação.")
+                print(SEM_PERMISSAO)
         case _:
             print("Você não digitou uma opção válida.")
+        case 11:
+            if e_dono:
+                listar_funcionarios()
+                while True:
+                    try:
+                        n = int(input("Escolha um funcionário para alterar seus dados: "))
+                        if n < 1 or n > len(funcionarios): raise Exception()
+                        # Ainda precisamos programar isso
+                    except:
+                        print("Você digitou algo errado. Tente novamente.")
+            else:
+                print(SEM_PERMISSAO)
+        case 12:
+            print("Exemplares emprestados:")
+            i = 1
+            for p in publicacoes:
+                for e in p.exemplares():
+                    if e.emprestado:
+                        print("[{i}] {p.tipo()} {p.titulo} ISBN {p.isbn}, emprestado por {e.funcionario} no dia {e.data_emprestimo}.")
+                        i += 1
+        case 13:
+            if e_dono:
+                print("Entre em contato com um dos clientes abaixo e dê baixa escolhendo uma das opções abaixo.")
+                while True:
+                    try:
+                        i = 1
+                        for m in multar:
+                            p = m[0]
+                            e = m[1]
+                            print(f"[{i}] {p.tipo()} {p.titulo}, ISBN {p.isbn}, emprestado por {e.funcionario} para {e.cliente.nome} com CPF {e.cliente.cpf} e número de telefone {e.cliente.telefone}, emprestado no dia {e.data_emprestimo} e devolvido no dia {e.data_devolucao}.")
+                        escolha = int(input("Qual dos exemplares você quer dar baixa? "))
+                        if escolha < 1 or escolha > len(multar): raise Exception()
+                        multar.remove(multar[n-1])
+                        print("Sucesso.")
+                    except:
+                        print("Você digitou algo errado. Tente novamente.")
+            else:
+                print(SEM_PERMISSAO)
     return escolha
 
 # Cadastrando o dono com a senha padrão
@@ -152,6 +213,7 @@ dono = Dono("Luiz de Moraes Sampaio", "426.704.238-17", "Guarulhos", "(11) 96061
 funcionarios = [dono]
 clientes = []
 publicacoes = []
+multar = []
 
 while True:
     funcionario_atual = fazer_login()
